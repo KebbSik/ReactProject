f8d72d9b7cda47701db010035eee839a8b6a8839
Kevin <kevinn.sarfoo1@gmail.com>
2024-11-14 19:30:48 +0100

    Build the color mode switch

--- game-hub/src/components/my/DarkMode/DarkModeToggle.tsx
+++ game-hub/src/components/my/DarkMode/DarkModeToggle.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import useDarkMode from "./useDarkMode";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Switch } from "@/components/ui/switch";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const DarkModeToggle: React.FC = () => {
   const [isDarkMode, toggleDarkMode] = useDarkMode();
 
   return (
-    <button onClick={toggleDarkMode} className="dark-mode-toggle">
-      {isDarkMode ? "Light Mode" : "Dark Mode"}
-    </button>
+    <>
+      {/* <button onClick={toggleDarkMode} className="dark-mode-toggle">
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </button> */}
+      <HStack>
+        {/*
+      Switch sizes:
+      -"xs"
+      -"sm"
+      -"md"
+      -"lg" */}
+        <Switch
+          // invalid
+          size="lg"
+          onCheckedChange={toggleDarkMode}
+          trackLabel={{
+            on: (
+              <Icon color="yellow.400">
+                <FaSun />
+              </Icon>
+            ),
+            off: (
+              <Icon color="gray.400">
+                <FaMoon />
+              </Icon>
+            ),
+          }}
+        ></Switch>
+        <Text>{isDarkMode ? "Dark Mode" : "Light Mode"}</Text>
+      </HStack>
+    </>
   );
 };
 
--- game-hub/src/components/my/NavBar.tsx
+++ game-hub/src/components/my/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "@/assets/logo.webp";
+import DarkModeToggle from "./DarkMode/DarkModeToggle";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding="10px">
       <Image src={logo} boxSize="60px" />
       <Text>This is my navbar</Text>
+      <DarkModeToggle />
     </HStack>
   );
 };
