5f57869331d958607360168f1ea33d718b98f11d
Kevin <kevinn.sarfoo1@gmail.com>
2024-11-23 15:28:15 +0100

    Showing a spinner

--- game-hub/src/components/my/GenreList.tsx
+++ game-hub/src/components/my/GenreList.tsx
@@ -1,24 +1,52 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, List, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Grid,
+  HStack,
+  Image,
+  List,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+
+  if (error) {
+    return null;
+  }
+
+  if (isLoading) {
+    return (
+      <Grid placeItems="center" paddingTop={5}>
+        <Spinner
+          size="xl"
+          borderWidth="5px"
+          color="green"
+          css={{ "--spinner-track-color": "colors.yellow.500" }}
+        />
+      </Grid>
+    );
+  }
   return (
-    <List.Root variant="plain">
-      {data.map((genre) => (
-        <List.Item key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            ></Image>
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <List.Root variant="plain">
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              ></Image>
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
